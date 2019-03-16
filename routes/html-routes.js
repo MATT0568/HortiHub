62fcac70037a17eb1c7b20f342690b4d73d2c93d
MATT0568 <lesniewiczm@vcu.edu>
2019-03-15 22:55:15 -0400

    fixed aws

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,15 +2,7 @@ var path = require("path");
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
-
-  app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/add.html"));
-  });
-
-  app.get("/all", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/all.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   app.get("/create", function(req, res) {
@@ -28,15 +20,4 @@ module.exports = function(app) {
   app.get("/plantinfo", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/plantinfo.html"));
   });
-};
-
-
-function requireLogin(req, res, next) {
-  if (!res.user) {
-    console.log(res.user);
-    res.redirect('/');
-  } else {
-    console.log(res.user);
-    next();
-  }
 };
